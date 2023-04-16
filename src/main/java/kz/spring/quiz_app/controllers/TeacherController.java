5d30c4d6bc5c37f88803c4657430fec1306f9c87
Tan3103 <tannur.uataev@mail.ru>
2023-04-16 17:49:35 +0600

    new

--- src/main/java/kz/spring/quiz_app/controllers/TeacherController.java
+++ src/main/java/kz/spring/quiz_app/controllers/TeacherController.java
@@ -14,6 +14,8 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/teacher")
 public class TeacherController {
